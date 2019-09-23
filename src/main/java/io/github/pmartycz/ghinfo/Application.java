cc786f7e8f6ab688b76b4b47789ad9a14ee9acf7
Piotr Martycz <pmartycz@gmail.com>
2019-09-24 00:09:19 +0200

    Configure Reactor Netty client

--- src/main/java/io/github/pmartycz/ghinfo/Application.java
+++ src/main/java/io/github/pmartycz/ghinfo/Application.java
@@ -1,10 +1,16 @@
 package io.github.pmartycz.ghinfo;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
 
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
@@ -25,6 +31,7 @@ public class Application implements WebClientCustomizer {
     public void customize(WebClient.Builder builder) {
         builder
                 .baseUrl(properties.getGithubUrl().toString())
+                .clientConnector(new ReactorClientHttpConnector(http()))
                 .defaultHeader("Accept", "application/vnd.github.v3+json");
 
         if (properties.getUsername() != null) {
@@ -32,6 +39,21 @@ public class Application implements WebClientCustomizer {
                     basicAuthentication(properties.getUsername(), properties.getPassword())
             );
         }
+    }
+
+    private HttpClient http() {
+        int timeout = 500; // hmm
+
+        return HttpClient.create()
+                .followRedirect(true) // As per https://developer.github.com/v3/#http-redirects
+                .compress(true) // those jsons you know they can get very long
+                .tcpConfiguration(tcp -> tcp
+                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeout)
+                        .doOnConnected(connection -> {
+                                connection.addHandlerLast(new ReadTimeoutHandler(timeout));
+                                connection.addHandlerLast(new WriteTimeoutHandler(timeout));
+                        })
+                );
     }
 
 }
